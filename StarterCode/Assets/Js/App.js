b194feea90425109d078b2badfd0a0b1b94c0b6e
manuelj22 <61300763+manuelj22@users.noreply.github.com>
2020-08-20 23:12:13 -0500

    .

--- StarterCode/Assets/Js/App.js
+++ StarterCode/Assets/Js/App.js
@@ -1,99 +1,94 @@
 
-var svgWidth = 960;
-var svgHeight = 500;
+let svgWidth = 960;
+let svgHeight = 500;
 
-var margin = {
+let margin = {
   top: 20,
   right: 40,
   bottom: 60,
   left: 100
 };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
 
 
-var svg = d3
+let svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
 
-var chartGroup = svg.append("g")
+let chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-d3.csv("./assets/data/data_data.csv", function(error, statedata) {  
-   
-    if (error) return console.warn(error)
-
-    statedata.forEach(function(data) {
-        data.poverty = +data.poverty;
-        data.smokes = +data.smokes;
-      });
+d3.csv("assets/data/data_data.csv").then(function (statedata) {
 
 
-  
-  var xScale = d3.scaleLinear()
-    .domain([8, d3.max(statedata, d => d.poverty)])
+  // if (error) return console.warn(error)
+
+  statedata.forEach(data => {
+    data.poverty = +data.poverty;
+    data.smokes = +data.smokes;
+  });
+
+
+
+  let xScale = d3.scaleLinear()
+    .domain([8, d3.max(statedata, data => data.poverty)])
     .range([0, width]);
 
-  var yLinearScale = d3.scaleLinear()
-    .domain([4, d3.max(statedata, d => d.smokes)])
+  let yLinearScale = d3.scaleLinear()
+    .domain([4, d3.max(statedata, data => data.smokes)])
     .range([height, 0]);
 
-  
-
-  var bottomAxis = d3.axisBottom(xScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
 
 
-  
-chartGroup.append("g")
+  let bottomAxis = d3.axisBottom(xScale);
+  let leftAxis = d3.axisLeft(yLinearScale);
+
+
+
+  chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
-  
-  
- 
-chartGroup.append("g")
+
+
+
+  chartGroup.append("g")
     .call(leftAxis);
 
 
-var circlesGroup = chartGroup.selectAll("circle")
+  let circlesGroup = chartGroup.selectAll("circle")
     .data(statedata)
     .enter()
     .append("circle")
-    .attr("cx", d => xScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("cx", data => xScale(data.poverty))
+    .attr("cy", data => yLinearScale(data.smokes))
     .attr("r", 15)
-    .attr("class", function(d) {
-        return "stateCircle " + d.abbr;
-      })
+    .attr("class", function (data) {
+      return "stateCircle " + data.abbr;
+    })
     .attr("fill", "purple")
     .attr("opacity", ".8")
 
-var toolTip = d3.tip()
-.attr("class", "tooltip")
-.attr([1, -1])
-.html(function(d) {
-  return (`${d.abbr}`);
-});
 
+  let textGroup = chartGroup.selectAll('text')
+    .exit()
+    .data(statedata)
+    .enter()
+    .append('text')
+    .text(data => data.abbr)
+    .attr('x', data => xScale(data.poverty))
+    .attr('y', data => yLinearScale(data.smokes))
+    .attr('text-anchor', 'middle')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '12px')
+    .attr('fill', '#fff');
 
-
-chartGroup.call(toolTip);
-
-circlesGroup.on("click", function(data) {
-    toolTip.show(data, this);
-  })
-   
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
-
-
-chartGroup.append("text")
+  chartGroup.append("text")
     .attr("transform", `translate(${width / 3}, ${height + margin.top + 20})`)
     .text("Percentage of Population in Poverty");
 
@@ -101,38 +96,31 @@ chartGroup.append("text")
   chartGroup.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height/1.2))
+    .attr("x", 0 - (height / 1.2))
     .attr("dy", "1em")
     .attr("class", "axisText")
     .text("Percentage of Population that Smokes");
 
+  let toolTip = d3.tip()
+    .attr('class', 'tooltip')
+    .offset([80, -60])
+    .html(function(data) {
+      return (`Poverty: ${data.poverty} <br> Smokes ${data.smokes}`);
+    });
+
+  chartGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function (data) {
+    toolTip.show(data, this);
+  })
+
+    .on("mouseout", function (data, index) {
+      toolTip.hide(data);
+    });
+
+
 
   
-let toolTip = d3.tip()
-      .attr('class', 'tooltip')
-      .offset([80, -60])
-      .html(function(d) {
-  return (`Poverty: ${d.poverty} <br> Healthcare ${d.healthcare}`);
 });
 
 
-chartGroup.call(toolTip);
-
-
-circlesGroup.on('mouseover', function(d) { 
-toolTip.show(d, this);
-})
-
-.on('mouseout', function(d,i) { 
-toolTip.hide(d);
-})
-
-}).catch(function(error) {
-console.log(error);
-});
-
-
-makeResponsive();
-
-
-d3.select(window).on('resize', makeResponsive);
